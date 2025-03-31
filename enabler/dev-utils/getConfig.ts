a3a67200b2f5fc1cd29ff248a975f1409d0c564e
James Morrison <james@frontastic.cloud>
2025-03-31 11:51:50 +0100

    core: remove VITE prefix from environment variables

--- enabler/dev-utils/getConfig.ts
+++ enabler/dev-utils/getConfig.ts
@@ -1,12 +1,12 @@
 export const getConfig = () => {
   const config = {
-    VITE_CTP_AUTH_URL: import.meta.env.VITE_CTP_AUTH_URL as string,
-    VITE_CTP_API_URL: import.meta.env.VITE_CTP_API_URL as string,
-    VITE_CTP_SESSION_URL: import.meta.env.VITE_CTP_SESSION_URL as string,
-    VITE_CTP_CLIENT_ID: import.meta.env.VITE_CTP_CLIENT_ID as string,
-    VITE_CTP_CLIENT_SECRET: import.meta.env.VITE_CTP_CLIENT_SECRET as string,
-    VITE_CTP_PROJECT_KEY: import.meta.env.VITE_CTP_PROJECT_KEY as string,
-    VITE_PROCESSOR_URL: import.meta.env.VITE_PROCESSOR_URL as string,
+    CTP_AUTH_URL: import.meta.env.VITE_CTP_AUTH_URL as string,
+    CTP_API_URL: import.meta.env.VITE_CTP_API_URL as string,
+    CTP_SESSION_URL: import.meta.env.VITE_CTP_SESSION_URL as string,
+    CTP_CLIENT_ID: import.meta.env.VITE_CTP_CLIENT_ID as string,
+    CTP_CLIENT_SECRET: import.meta.env.VITE_CTP_CLIENT_SECRET as string,
+    CTP_PROJECT_KEY: import.meta.env.VITE_CTP_PROJECT_KEY as string,
+    PROCESSOR_URL: import.meta.env.VITE_PROCESSOR_URL as string,
   };
   Object.keys(config).forEach((key) => {
     if (!config[key as keyof typeof config]) {
