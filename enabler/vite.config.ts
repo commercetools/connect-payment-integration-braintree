f779dcccde30ce0493cbe574e3a66c4e13199999
James Morrison <james@frontastic.cloud>
2025-04-07 10:40:21 +0100

    fix: enabled hot reload on Windows through docker compose

--- enabler/vite.config.ts
+++ enabler/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
+  server: { watch: { usePolling: true } },
   css: {
     preprocessorOptions: {
       scss: {
