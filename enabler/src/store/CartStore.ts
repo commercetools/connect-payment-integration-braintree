7558e6d3d980430336719198bb764cc2a5742252
James Morrison <james.morrison@commercetools.com>
2025-04-09 15:36:43 +0100

    feat: added basic data store for cartId and session

---
+++ enabler/src/store/CartStore.ts
@@ -0,0 +1,33 @@
+import { exhaustiveMatchingGuard } from "./exhaustiveMatchingGuard";
+import { Store } from "./Store";
+
+type ACTION =
+  | {
+      type: "SET_CART_ID";
+      cartId: string;
+    }
+  | {
+      type: "REMOVE_CART_ID";
+    };
+
+const cartIdLocalStorageKey = "cartId";
+
+const cartStore = new Store<string | undefined, ACTION>(
+  (action, _, setState) => {
+    switch (action.type) {
+      case "SET_CART_ID":
+        localStorage.setItem(cartIdLocalStorageKey, action.cartId);
+        setState(action.cartId);
+        break;
+      case "REMOVE_CART_ID":
+        localStorage.removeItem(cartIdLocalStorageKey);
+        setState(undefined);
+        break;
+      default:
+        exhaustiveMatchingGuard(action);
+    }
+  },
+  localStorage.getItem(cartIdLocalStorageKey) ?? undefined
+);
+
+export default cartStore;
