13701f1e22cbe930a6bafcf22b25999f82aaf84d
James Morrison <james@frontastic.cloud>
2025-04-09 11:29:31 +0100

    chore: removed Braintree processor env vars

--- enabler/dev-utils/getConfig.ts
+++ enabler/dev-utils/getConfig.ts
@@ -6,16 +6,12 @@ const config = {
   CTP_CLIENT_SECRET: import.meta.env.VITE_CTP_CLIENT_SECRET as string,
   CTP_PROJECT_KEY: import.meta.env.VITE_CTP_PROJECT_KEY as string,
   PROCESSOR_URL: import.meta.env.VITE_PROCESSOR_URL as string,
-  // TODO: evaluate need for below
-  BRAINTREE_MERCHANT_ID: import.meta.env.VITE_BRAINTREE_MERCHANT_ID as string,
-  BRAINTREE_PUBLIC_KEY: import.meta.env.VITE_BRAINTREE_PUBLIC_KEY as string,
-  BRAINTREE_PRIVATE_KEY: import.meta.env.VITE_BRAINTREE_PRIVATE_KEY as string,
 };
 
-Object.keys(config).forEach((key) => {
+for (const key in config) {
   if (!config[key as keyof typeof config]) {
     console.warn(`Config parameter "${key}" is undefined.`);
   }
-});
+}
 
 export const getConfig = () => config;
