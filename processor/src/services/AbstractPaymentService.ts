70e66d3c5bb87a10ea390f5f0e229bd521db2967
King-Hin Leung <kinghin.leung@commercetools.com>
2025-07-24 10:30:53 +0200

    fix(processor): fix health check function

--- processor/src/services/AbstractPaymentService.ts
+++ processor/src/services/AbstractPaymentService.ts
@@ -16,11 +16,7 @@ import {
 	ReversePaymentRequest,
 	StatusResponse,
 } from "./types/operations";
-import {
-	SupportedPaymentComponentsSchemaDTO,
-	AmountSchemaDTO,
-	PaymentModificationStatus,
-} from "../dtos/operations";
+import { SupportedPaymentComponentsSchemaDTO, AmountSchemaDTO, PaymentModificationStatus } from "../dtos/operations";
 
 import { logger } from "../libs/logger";
 import { PaymentTransactionTypes } from "./types/operations/PaymentTransactionTypes";
