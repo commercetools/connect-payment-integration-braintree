da69bd7d297fdac518e051b3c3752829943015f3
King-Hin Leung <kinghin.leung@commercetools.com>
2025-09-09 13:58:34 +0200

    fix(enabler): add bootstrap.min.css inside card component

--- enabler/src/components/payment-methods/card/Card.ts
+++ enabler/src/components/payment-methods/card/Card.ts
@@ -150,7 +150,7 @@ export class Card extends BaseComponent {
 	}
 	private _getTemplate() {
 		return `<!-- Bootstrap inspired Braintree Hosted Fields example -->
-				
+				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 				<style>
 					.form-control {
 						height: calc(1.5em + .75rem + 2px);
